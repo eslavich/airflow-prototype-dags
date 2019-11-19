6356926ac8a46e1a7608432a1a2ff23dcaf1380b
Ed Slavich <eslavich@stsci.edu>
2019-11-19 11:40:48 -0500

    Add slack test DAG

---
+++ dags/DAG_slack_test.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2019, 1, 1),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(seconds=30)
+}
+
+dag = DAG("slack-test", default_args=default_args, schedule_interval=None)
+
+task = SlackWebhookOperator(
+    http_conn_id="slack-alerts",
+    message="testing..."
+)
